2c41f62f9266df50e0008fe7f67253d7330cba68
Stephen OBrien <wayofthepie@gmail.com>
2020-12-05 10:36:38 +0000

    Refactor day5 part1

--- src/day5.rs
+++ src/day5.rs
@@ -4,32 +4,26 @@ pub mod part_one {
             .lines()
             .map(|line| {
                 let (row_details, col_details) = line.split_at(7);
-                let row = row_details
-                    .chars()
-                    .fold((0, 127), |(min, max), letter| {
-                        let mid = min + ((max - min) / 2);
-                        match letter {
-                            'F' => (min, mid),
-                            'B' => (mid + 1, max),
-                            _ => unreachable!(),
-                        }
-                    })
-                    .0;
-                let col = col_details
-                    .chars()
-                    .fold((0, 7), |(min, max), letter| {
-                        let mid = min + ((max - min) / 2);
-                        match letter {
-                            'L' => (min, mid),
-                            'R' => (mid + 1, max),
-                            _ => unreachable!(),
-                        }
-                    })
-                    .0;
+                let row = compute_position(row_details, 'F', 0, 127) as usize;
+                let col = compute_position(col_details, 'L', 0, 7) as usize;
                 row * 8 + col
             })
             .max()
             .unwrap_or(0)
+    }
+
+    fn compute_position(definition: &str, left: char, min: u8, max: u8) -> u8 {
+        definition
+            .chars()
+            .fold((min, max), |(min, max), letter| {
+                let mid = min + ((max - min) / 2);
+                if letter == left {
+                    (min, mid)
+                } else {
+                    (mid + 1, max)
+                }
+            })
+            .0
     }
 }
 #[cfg(test)]
