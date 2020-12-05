bb044fe931dea56206a487207dbfb162e376db32
Stephen OBrien <wayofthepie@gmail.com>
2020-12-05 12:11:22 +0000

    Refactor day5

--- src/day5.rs
+++ src/day5.rs
@@ -1,33 +1,16 @@
 pub mod part_one {
-    use super::compute_position;
+    use super::compute_positions;
 
     pub fn solution(input: &str) -> usize {
-        input
-            .lines()
-            .map(|line| {
-                let (row_details, col_details) = line.split_at(7);
-                let row = compute_position(row_details, 'F', 0, 127) as usize;
-                let col = compute_position(col_details, 'L', 0, 7) as usize;
-                row * 8 + col
-            })
-            .max()
-            .unwrap_or(0)
+        compute_positions(input).max().unwrap_or(0)
     }
 }
 
 pub mod part_two {
-    use super::compute_position;
+    use super::compute_positions;
 
     pub fn solution(input: &str) -> usize {
-        let mut ids = input
-            .lines()
-            .map(|line| {
-                let (row_details, col_details) = line.split_at(7);
-                let row = compute_position(row_details, 'F', 0, 127) as usize;
-                let col = compute_position(col_details, 'L', 0, 7) as usize;
-                row * 8 + col
-            })
-            .collect::<Vec<usize>>();
+        let mut ids = compute_positions(input).collect::<Vec<usize>>();
         ids.sort_unstable();
         ids.windows(2)
             .find(|window| window[0] + 1 < window[1])
@@ -37,18 +20,26 @@ pub mod part_two {
     }
 }
 
-fn compute_position(definition: &str, left: char, min: u8, max: u8) -> u8 {
+fn compute_positions<'input>(input: &'input str) -> impl Iterator<Item = usize> + 'input {
+    input
+        .lines()
+        .map(compute_row_and_col)
+        .map(|(row, col)| row as usize * 8 + col as usize)
+}
+
+fn compute_row_and_col(line: &str) -> (u8, u8) {
+    let (row_details, col_details) = line.split_at(7);
+    (
+        compute_position(row_details, 'F', &[64, 32, 16, 8, 4, 2, 1]),
+        compute_position(col_details, 'L', &[4, 2, 1]),
+    )
+}
+
+fn compute_position(definition: &str, left: char, mapping: &[u8]) -> u8 {
     definition
         .chars()
-        .fold((min, max), |(min, max), letter| {
-            let mid = min + ((max - min) / 2);
-            if letter == left {
-                (min, mid)
-            } else {
-                (mid + 1, max)
-            }
-        })
-        .0
+        .zip(mapping)
+        .fold(0, |acc, (ch, &n)| if ch == left { acc } else { acc + n })
 }
 
 #[cfg(test)]
