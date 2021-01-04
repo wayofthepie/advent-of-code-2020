5ed7d6468bbc175698efd058316c72dcf6353752
Stephen OBrien <wayofthepie@gmail.com>
2021-01-04 11:14:23 +0000

    Add solution to day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -16,13 +16,30 @@ pub mod part_one {
     }
 }
 
+pub mod part_two {
+    pub fn solution(input: &str) -> usize {
+        let groups = input
+            .split("\n\n")
+            .map(|s| s.split('\n').collect())
+            .collect::<Vec<Vec<&str>>>();
+        let mut sum = 0;
+        for group in groups.iter() {
+            let first = group.first().unwrap_or(&"");
+            for choice in first.chars() {
+                if group.iter().all(|other| other.contains(choice)) {
+                    sum += 1;
+                }
+            }
+        }
+        sum
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use super::part_one;
+    use super::{part_one, part_two};
 
-    #[test]
-    pub fn part_one_solution_for_example() {
-        let input = r#"abc
+    const EXAMPLE: &str = r#"abc
 
 a
 b
@@ -37,7 +54,10 @@ a
 a
 
 b"#;
-        let result = part_one::solution(input);
+
+    #[test]
+    pub fn part_one_solution_for_example() {
+        let result = part_one::solution(EXAMPLE);
         assert_eq!(11, result);
     }
 
@@ -46,5 +66,18 @@ b"#;
         let input = include_str!("../resources/day6.txt");
         let result = part_one::solution(input);
         assert_eq!(6596, result);
+    }
+
+    #[test]
+    pub fn part_two_solution_for_example() {
+        let result = part_two::solution(EXAMPLE);
+        assert_eq!(6, result);
+    }
+
+    #[test]
+    pub fn part_two_solution() {
+        let input = include_str!("../resources/day6.txt");
+        let result = part_two::solution(input);
+        assert_eq!(3219, result);
     }
 }
